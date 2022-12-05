ee2fff6b4515c67825222768692369f926d60619
nisan1990 <nsn11ster@gmail.com>
2022-12-05 16:54:04 +0200

    * install angular 14 * start build client side

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UsersService } from './services/users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users: Observable<any>;
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.users = this.usersService.getUsers();
+  }
+}
