3e4389fbb177b3a606887c49ef7eba18452384ed
nisan1990 <nsn11ster@gmail.com>
2022-12-12 18:09:45 +0200

    * passwordHash * JWT

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,11 +14,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,4 +16,5 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.users = this.usersService.getUsers();
   }
+
 }
